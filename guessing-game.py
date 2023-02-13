98daaeaa343d1631a8c61d0ac2ae29e8b8094913
saku-1031 <aidea.sensei@gmail.com>
2023-02-13 18:10:04 +0900

    数当てゲームに答えるまでの回数の採点機能,ヒント機能を追加

--- guessing-game.py
+++ guessing-game.py
@@ -8,6 +8,12 @@ while fin == 0:
     intans = input("答えはなんだと思いますか？")
     if int(intans) == ans:
         print("正解！！答えは" + str(ans) + "です。\n今回は" + str(count) + "回間違えました")
+        if count <= 5:
+            print("平均より少ないです。")
+        elif count >= 9:
+            print("平均より多いです。")
+        else:
+            print("平均的です。")
         fin = 1
     else:
         if ans < int(intans):
@@ -16,3 +22,8 @@ while fin == 0:
         elif ans > int(intans):
             print("答えは" + str(intans) + "よりも多いです。")
             count += 1
+        challenge = random.randint(1, 10)
+        if challenge == 1:
+            ans_list = [random.randint(1, 100), ans, random.randint(1, 100)]
+            random.shuffle(ans_list)
+            print("challenge！！ 答えは" + str(ans_list) + "のどれかです。")
